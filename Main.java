53713de7b573afe265f5e72059f513da2de57680
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-04-22 18:45:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,31 +8,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        FileWriter fileWriter = new FileWriter("output.txt");
         String regexp1 = "(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\D(\\d{1,2}))))\\.((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){2}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|((\\d{1,2})\\D)))";
         Pattern pattern = Pattern.compile(regexp1);
         Scanner in = new Scanner(System.in);
         System.out.print("Введите предложение: ");
         String word = in.nextLine();
         Matcher m = pattern.matcher(word);
-        if(m.find()){
-            try{
-                String result = "Корректный IP адрес: " + m.group();
-                fileWriter.write(result);
-                fileWriter.flush();
+        try{
+            FileWriter fileWriter = new FileWriter("output.txt");
+            if(m.find()){
+                try{
+                    String result = "Корректный IP адрес: " + m.group();
+                    fileWriter.write(result);
+                    fileWriter.flush();
+                }
+                catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
             }
-            catch(IOException ex){
-                System.out.println(ex.getMessage());
+            else{
+                try {
+                    fileWriter.write("Корректные IP-адресы не найдены.");
+                    fileWriter.flush();
+                }
+                catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
             }
         }
-        else{
-            try {
-                fileWriter.write("Корректные IP-адресы не найдены.");
-                fileWriter.flush();
-            }
-            catch(IOException ex){
-                System.out.println(ex.getMessage());
-            }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 }
